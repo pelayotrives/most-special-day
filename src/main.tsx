f2c1c1f7a426ea23cac915ebed73c32c7d397637
Pelayo Trives <pelayotrives@gmail.com>
2024-11-18 03:12:01 +0100

    feat: first components created.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import './index.css';
+import App from './App.tsx';
+
+const Main = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+    });
+  }, []);
+
+  return <App />;
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Main />
+  </StrictMode>
+);
